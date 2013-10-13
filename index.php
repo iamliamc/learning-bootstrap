3483b67ce86ae909aa9e3edd5a685627856df98d
Liam J. Considine <ljc3179@gmail.com>
2013-10-13 13:57:19 -0400

    Added basic Index file

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+	<title>Learning-Bootstrap</title>
+</head>
+<body>
+ <p>Welcome to Liam's Learning Bootstrap</p>
+</body>
+</html>
