3ad84ed90efea691c5a00c147fb0a4b42dbf29ff
Liam J. Considine <ljc3179@gmail.com>
2013-10-13 17:52:06 -0400

    Installed Bootstrap Template

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8" />
-	<title>Learning-Bootstrap</title>
-</head>
-<body>
- <p>Welcome to Liam's Learning Bootstrap</p>
-</body>
+<html>
+  <head>
+    <title>Liam Bootstrap Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="_/css/mystyles.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+    <h1>Hello, world!</h1
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="_/js/bootstrap.min.js"></script>
+    <script src="_/js/myscript.min.js"></script>
+  </body>
 </html>
